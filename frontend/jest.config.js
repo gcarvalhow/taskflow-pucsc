bbbf474c118a468f687e2d8651e3e41acb9a189e
gabrielcarvallho <gabriellccarvalho.dev@gmail.com>
2025-10-22 01:21:51 -0300

    KAN-18 Refactor CI/CD workflows and add frontend testing setup.

---
+++ frontend/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: 'tsconfig.json' }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  testMatch: ['**/__tests__/**/*.(ts|tsx|js)'],
+};
